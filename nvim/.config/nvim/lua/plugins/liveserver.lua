88ba8eb5c98bd61a28c1d9189f0d71b43757ca20
Floranaras <carlosranara0@gmail.com>
2026-02-13 06:10:40 +0800

    feat: convert script and liveserver config changes

--- nvim/.config/nvim/lua/plugins/liveserver.lua
+++ nvim/.config/nvim/lua/plugins/liveserver.lua
@@ -3,16 +3,18 @@ return {
         "barrett-ruth/live-server.nvim",
         build = "npm install -g live-server",
         config = function()
-            require("live-server").setup({
+            -- New configuration method
+            vim.g.live_server = {
                 args = { "--port=8080", "--no-css-inject" }
-            })
+            }
 
+            -- Keymaps
             vim.keymap.set("n", "<leader>ls", ":LiveServerStart<CR>", { desc = "Start Live Server" })
             vim.keymap.set("n", "<leader>lc", ":LiveServerStop<CR>", { desc = "Stop Live Server" })
 
-            -- Auto-save frequently for seamless updates
+            -- Auto-save for seamless updates
             vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
-                pattern = { "*.html", "*.css", "*.js", "*.rs"},
+                pattern = { "*.html", "*.css", "*.js", "*.rs" },
                 callback = function()
                     vim.cmd("silent! write")
                 end,
