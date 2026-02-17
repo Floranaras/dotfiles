f8b1c8aac7a4d10b59131e13a6b7365141a7bc65
Floranaras <carlosranara0@gmail.com>
2026-02-17 23:43:53 +0800

    fix: liveserver defaulting to safari

--- nvim/.config/nvim/lua/plugins/liveserver.lua
+++ nvim/.config/nvim/lua/plugins/liveserver.lua
@@ -3,16 +3,13 @@ return {
         "barrett-ruth/live-server.nvim",
         build = "npm install -g live-server",
         config = function()
-            -- New configuration method
             vim.g.live_server = {
-                args = { "--port=8080", "--no-css-inject" }
+                args = { "--port=8080", "--no-css-inject", "--browser=brave browser"}
             }
 
-            -- Keymaps
             vim.keymap.set("n", "<leader>ls", ":LiveServerStart<CR>", { desc = "Start Live Server" })
             vim.keymap.set("n", "<leader>lc", ":LiveServerStop<CR>", { desc = "Stop Live Server" })
 
-            -- Auto-save for seamless updates
             vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
                 pattern = { "*.html", "*.css", "*.js", "*.rs" },
                 callback = function()
