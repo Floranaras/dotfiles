e1bd25f739e3a55464897ae4809473249cbd198e
Amane Kai / 雨音カイ <carlosranara0@gmail.com>
2026-05-02 10:49:32 +0800

    chore: trim verbose doc comments

--- nvim/.config/nvim/lua/plugins/emmet.lua
+++ nvim/.config/nvim/lua/plugins/emmet.lua
@@ -1,30 +1,10 @@
---- @file emmet.lua
---- @brief Emmet abbreviation expansion for faster HTML/CSS/JSX workflow.
---- This module configures the Emmet leader key and enables JSX support
---- for JavaScript buffers.
-
 return {
   "mattn/emmet-vim",
-  -- Supported filetypes for Emmet expansion
-  ft = {
-    "html",
-    "css",
-    "javascript",
-    "javascriptreact",
-    "typescriptreact",
-  },
-  --- @brief Configure global Emmet settings.
+  ft = { "html", "css", "javascript", "javascriptreact", "typescriptreact" },
   config = function()
-    -- Set the expansion leader key to <C-y>
-    -- Default trigger is <C-y>, (comma)
     vim.g.user_emmet_leader_key = "<C-y>"
-
-    -- Configure specific language extensions
     vim.g.user_emmet_settings = {
-      javascript = {
-        -- Treat standard JS files as JSX for Emmet expansion
-        extends = "jsx",
-      },
+      javascript = { extends = "jsx" },
     }
   end,
 }
