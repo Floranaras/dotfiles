02f1a9200a71fab66c99a92d61fc5ba439067b3e
Amane Kai / 雨音カイ <carlosranara0@gmail.com>
2026-05-02 10:49:32 +0800

    feat: migrate to harpoon v2 API

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -1,29 +1,17 @@
---- @file harpoon.lua
---- @brief Rapid file navigation and marking system.
---- Harpoon allows you to pin specific files to a persistent menu and 
---- jump between them instantly using dedicated hotkeys.
-
 return {
-  "theprimeagen/harpoon",
-  --- @brief Configures markers and navigation shortcuts for Harpoon.
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
-    local mark = require("harpoon.mark")
-    local ui = require("harpoon.ui")
+    local harpoon = require("harpoon")
+    harpoon:setup()
 
-    -- 1. MARKING & UI
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<C-e>",     function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    -- Mark the current file for the Harpoon list.
-    vim.keymap.set("n", "<leader>a", mark.add_file)
-
-    -- Toggle the visual menu to view and manage marked files.
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-    -- 2. FAST NAVIGATION
-
-    -- Navigate directly to specific marks (1 through 4).
-    vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-    vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end)
-    vim.keymap.set("n", "<C-s>", function() ui.nav_file(3) end)
-    vim.keymap.set("n", "<C-t>", function() ui.nav_file(4) end)
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-n>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-t>", function() harpoon:list():select(4) end)
   end,
 }
